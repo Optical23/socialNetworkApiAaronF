34c82ec08d0de593e99b91269e59d53ac639dad7
Optical23 <aaroneferguson@gmail.com>
2022-03-11 15:33:55 -0600

    thoughtSchema created

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const opts = { toJSON: { virtual: true, getters: true }, id: false };
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Thought content is Required',
+            validate: [({ length }) => length <= 280, "Thought content can't be longer than 280 characters"]
+        },
+        createdAt : {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Username is Required'
+        },
+        reactions: [reactionSchema]
+    }, opts
+);
+
+
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return 
+})
+
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+module.exports = Thoughts;
