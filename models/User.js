6a1fa69a542d092911f206276a687b787520a097
Optical23 <aaroneferguson@gmail.com>
2022-03-11 15:16:03 -0600

    user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,32 @@
 const { Schema, model } = require('mongoose');
 
+const opts = { toJSON: { virtual: true }, id: false };
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: 'Username is Required'
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email is Required',
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: {
 
+        },
+        friends: {
 
+        }
+    }, opts
+);
 
+UserSchema.virtual('friendCount').get(function() {
+    return 
+})
 
 const User = model('User', UserSchema);
 module.exports = User;
