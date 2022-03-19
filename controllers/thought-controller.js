d657c2727efd194948d1b35a395613dc81957a79
Optical23 <aaron@thefergusons.com>
2022-03-19 16:42:26 -0500

    created base thought routes and functions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,74 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
+const { db } = require('../models/User');
+
+const thoughtController = {
+    // get all Thoughts
+    getAllThoughts(req, res) {
+      Thought.find({})
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+  
+    // get one Thought by id
+    getThoughtById({ params }, res) {
+      Thought.findOne({ _id: params.id })
+        .select('-__v')
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+  
+    // createThought
+    createThought({ body }, res) {
+      Thought.create(body)
+        .then(dbThoughtData => {
+            User.findOneAndUpdate(
+                {
+                    username: dbThoughtData.username
+                }, 
+                {
+                    $push: {
+                        thoughts: dbThoughtData._id
+                    }
+                })
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                    }
+                })
+                .catch(err => res.json(err));
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err));
+    },
+  
+    // update Thought by id
+    updateThought({ params, body }, res) {
+      Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+          if (!dbThoughtData) {
+            res.status(404).json({ message: 'No Thought found with this id!' });
+            return;
+          }
+          res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+  
+    // delete Thought
+    deleteThought({ params }, res) {
+      Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
+    },
+}
+
+module.exports = thoughtController
